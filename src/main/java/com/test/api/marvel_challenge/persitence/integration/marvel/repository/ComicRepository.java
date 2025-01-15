3f36c301ee1f9465ddbacb685e0e2b2272ab3a9b
José Pérez <105478390+JoseDPerez11@users.noreply.github.com>
2025-01-15 13:19:27 -0500

    working repositories for characters and comics

--- src/main/java/com/test/api/marvel_challenge/persitence/integration/marvel/repository/ComicRepository.java
+++ src/main/java/com/test/api/marvel_challenge/persitence/integration/marvel/repository/ComicRepository.java
@@ -1,18 +1,57 @@
 package com.test.api.marvel_challenge.persitence.integration.marvel.repository;
 
+import com.fasterxml.jackson.databind.JsonNode;
 import com.test.api.marvel_challenge.dto.MyPageable;
+import com.test.api.marvel_challenge.persitence.integration.marvel.MarvelAPIConfig;
 import com.test.api.marvel_challenge.persitence.integration.marvel.dto.ComicDto;
+import jakarta.annotation.PostConstruct;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Repository;
 
 import java.util.List;
+import java.util.Map;
 
 @Repository
 public class ComicRepository {
+
+    @Autowired
+    private MarvelAPIConfig marvelAPIConfig;
+
+    @Value("${integration.marvel.base-path}")
+    private String basePath;
+
+    private String comicPath;
+
+    @PostConstruct
+    private void setPath() {
+        characterPath = basePath.concat("/").concat("comics");
+    }
+
     public List<ComicDto> findAll(MyPageable pageable, Long characterId) {
-        return null;
+        Map<String, String> marvelQueryParams = getQueryParamsForFindAll(pageable, characterId);
+        JsonNode response = httpClientService.doGet(comicPath, marvelQueryParams, JsonNode.class);
+
+        return ComicMapper.toDtoList(response);
+    }
+
+    private Map<String, String> getQueryParamsForFindAll(MyPageable pageable, Long characterId) {
+        Map<String, String> marvelQueryParams = marvelAPIConfig.getAuthenticationQueryParams();
+
+        marvelQueryParams.put("offset", Long.toString(pageable.offset()));
+        marvelQueryParams.put("limit", Long.toString(pageable.limit()));
+
+        if (characterId != null && characterId.longValue() > 0) {
+            marvelQueryParams.put("characters", Long.toString(characterId));
+        }
+        return marvelQueryParams;
     }
 
     public ComicDto findById(Long comicId) {
-        return null;
+        Map<String, String> marvelQueryParams = marvelAPIConfig.getAuthenticationQueryParams();
+        String finalUrl = comicPath.concat("/").concat(Long.toString(comicId));
+        JsonNode response = httpClientService.doGet(finalUrl, marvelQueryParams, JsonNode.class);
+
+        return ComicMapper.toDtoList(response).get(0);
     }
 }
